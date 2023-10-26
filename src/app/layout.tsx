614dd55badc3978d484731e67345dbc21871a8f3
shindaeho <sdh077@urbancorp.co.kr>
2023-10-26 15:32:50 +0900

    flowbite 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Providers } from '@/redux/provider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Menu from './Menu'
-
+import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
